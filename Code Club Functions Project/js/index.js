5dbd2c5109987c9ad3402ea1ffe975b60df5b66b
Garrett Smith <19smitgr@users.noreply.github.com>
2016-06-11 13:43:26 -0400

    initial commit

---
+++ Code Club Functions Project/js/index.js
@@ -0,0 +1,35 @@
+var usr_word;
+var pigla = "ay";
+var output;
+
+$(document).ready(function(){
+  
+  $(".button-submit").on('click',function(){ 
+    usr_word = $(this).parent().children('.input-word').text(); 
+   
+    if (usr_word.length > 0 &&  usr_word.match(/^[a-zA-Z][a-zA-Z]+$/) !== null) {
+    $(".result-text").css('background-color','transparent');
+		var vowels = ["a","e","i","o","u"];
+		var first = usr_word.slice(0,1);
+		var sLast = usr_word.slice(1);
+
+			if (first === "a" || first === "e" || first === "i" || first === "o" || first === "u") {
+				output = usr_word + pigla;
+				$(".result-text").text(output);
+			}
+
+			else {
+				output = sLast + first + pigla;
+				$(".result-text").text(output);
+			}
+	}		
+		
+    else {
+    $(".result-text").text("Enter a valid word!");
+    $(".result-text").css('backgroundColor','#B63434');
+    }
+    
+  });
+
+
+});
